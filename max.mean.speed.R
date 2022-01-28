1067c5fb47c2be26529e5f122a11a1f0d581fc05
Aaron Pearson <46941570+aaronzpearson@users.noreply.github.com>
2022-01-28 08:06:39 -0700

    separate max.median.speed from max.mean.speed

--- max.mean.speed.R
+++ max.mean.speed.R
@@ -13,31 +13,6 @@ max.mean.speed <- function(x, n = 600) {
   max.mean.vel
 }
 
-max.median.speed <- function(x, n = "roeker") {
-  
-  if(n == "roeker") {
-    n = c(0.3, 0.5, 1, 2, 3, 4, 
-          5, 6.5, 10, 13.5, 18, 
-          30, 60, 120, 300, 600, 
-          900, 1200, 1800, 2400, 
-          2700)
-    } else {
-     n = n
-   }
-  
-  max.median.vel <- do.call(
-    rbind,
-    lapply(
-      data.table::frollapply(x, n = n, FUN = Rfast::med),
-      max, na.rm = TRUE)
-  )
-  
-  invisible(gc)
-  
-  max.median.vel
-  
-}
-
 
 # builds a data.frame from the max.mean.speed function
 # used in the crit.speed functions
