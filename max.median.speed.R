cd76a26bb06413eba8b04833b8fcc74e85deb15d
Aaron Pearson <46941570+aaronzpearson@users.noreply.github.com>
2022-01-28 08:08:14 -0700

    update max.median.speed with tryCatch
    Function is built for 10Hz data. If data is not 10Hz, function now rounds "roeker" to best match original algorithm

---
+++ max.median.speed.R
@@ -0,0 +1,56 @@
+max.median.speed. <- function(x, n = "roeker", sample.rate = 10) {
+
+  if(n == "roeker") {
+
+    n. = c(0.3, 0.5, 1, 2, 3, 4,
+           5, 6.5, 10, 13.5, 18,
+           30, 60, 120, 300, 600,
+           900, 1200, 1800, 2400,
+           2700) * sample.rate
+
+  } else {
+
+    n. = n
+
+  }
+
+
+  tryCatch(
+
+    expr = {
+
+      do.call(
+        rbind,
+        lapply(
+          data.table::frollapply(x, n = n., FUN = Rfast::med),
+          max, na.rm = TRUE
+        )
+      )
+
+    },
+
+    error = function(error_condition){
+
+      message("This function is written for 10 Hz data; n was rounded to handle errors.")
+
+    },
+
+    finally = {
+
+      n. = round(n., 0)
+
+      max.median.vel <- do.call(
+        rbind,
+        lapply(
+          data.table::frollapply(x, n = n., FUN = Rfast::med),
+          max, na.rm = TRUE)
+      )
+
+    }
+  )
+
+  invisible(gc)
+
+  max.median.vel
+
+}
