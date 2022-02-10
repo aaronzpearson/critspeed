b875f39e51a3849f3afa32a45ff33419aaf2a084
Aaron Pearson <46941570+aaronzpearson@users.noreply.github.com>
2022-02-10 15:35:06 -0700

    include global.data argument for weak computers

--- cs.results.observed.list.R
+++ cs.results.observed.list.R
@@ -1,28 +1,24 @@
 cs.results.observed.list <- function(data = list(),
                                   data2 = list(),
                                   player.speed,
+                                  global.data = FALSE,
                                   sample.rate = 10,
                                   dur = 600) {
 
   temp.df <- as.list(data)[[1]] == player.speed
   speed.col.name <- names(which(colSums(temp.df) > 0))
 
+  if(global.data == TRUE) {set.one.max.mean <- do.call(rbind, data)}
+  if(global.data == TRUE) {set.two.max.mean <- do.call(rbind, data2)}
+
+
+  if(global.data == FALSE) {
+
   set.one.max.mean <- do.call(rbind,
                               lapply(data,
                                      function(x) data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
                                                             max.mean.speed = max.mean.speed(x[, speed.col.name],
                                                                                             n = dur*sample.rate)))
-  )
-
-  set.one.max.mean.global <- do.call(rbind,
-                                     lapply(
-                                       split(
-                                         set.one.max.mean, as.factor(set.one.max.mean$duration)
-                                       ),
-                                       function(x) {
-                                         return(x[which.max(x$max.mean.speed),])
-                                       }
-                                     )
   )
 
   set.two.max.mean <- do.call(rbind,
@@ -32,13 +28,26 @@ cs.results.observed.list <- function(data = list(),
                                                                                             n = dur*sample.rate)))
   )
 
+  }
+
+  set.one.max.mean.global <- do.call(rbind,
+                                     lapply(
+                                       split(
+                                         set.one.max.mean, as.factor(set.one.max.mean$duration)
+                                       ),
+                                       function(x) {
+                                         return(x[which.max(x$max.mean.speed), ])
+                                       }
+                                     )
+  )
+
   set.two.max.mean.global <- do.call(rbind,
                                      lapply(
                                        split(
                                          set.two.max.mean, as.factor(set.two.max.mean$duration)
                                        ),
                                        function(x) {
-                                         return(x[which.max(x$max.mean.speed),])
+                                         return(x[which.max(x$max.mean.speed), ])
                                        }
                                      )
   )
