1dba9e9d0eda2a19ecf05f06a0b23fc2dbfe5e65
Aaron Pearson <46941570+aaronzpearson@users.noreply.github.com>
2022-02-15 12:12:22 -0500

    needs fewer resources, speed improvement
    last step: moved away from split-apply-combine for data.table syntax, 
    global.data == FALSE: replaced do.call(rbind, lapply...) w/ for loop to decrease resources (previous code is commented for referencing) 
    global.data == TRUE: improved lapply function for greater efficiency
    
    heavier reliance on c++ backend and lowered RAM & CPU requirements = improved function speed

--- cs.results.observed.list.R
+++ cs.results.observed.list.R
@@ -1,90 +1,101 @@
 cs.results.observed.list <- function(data = list(),
-                                  data2 = list(),
-                                  player.speed,
-                                  global.data = FALSE,
-                                  sample.rate = 10,
-                                  dur = 600) {
-
-  temp.df <- as.list(data)[[1]] == player.speed
-  speed.col.name <- names(which(colSums(temp.df) > 0))
-
-  if(global.data == TRUE) {
-
-
-    data.df <- do.call(rbind, data)
-
-    set.one.max.mean <- data.frame(duration =
-                                     rep(
-                                       seq(0.1,
-                                           max(as.double(lapply(data, FUN = nrow)))/sample.rate,
-                                           by = 1/sample.rate),
-                                       times = length(data)
-                                     )[1:nrow(data.df)],
-                                   max.mean.speed = data.df[, speed.col.name]
-    )
-
-    colnames(set.one.max.mean) <- c("duration", "max.mean.speed")
-
-    data2.df <- do.call(rbind, data2)
-
-    set.two.max.mean <- data.frame(duration =
-                                     rep(
-                                       seq(0.1,
-                                           max(as.double(lapply(data2, FUN = nrow)))/sample.rate,
-                                           by = 1/sample.rate),
-                                       times = length(data2)
-                                     )[1:nrow(data2.df)],
-                                   max.mean.speed = data2.df[, speed.col.name]
-    )
-
-    colnames(set.two.max.mean) <- c("duration", "max.mean.speed")
-
-    }
-
-
-  if(global.data == FALSE) {
-
-  set.one.max.mean <- do.call(rbind,
-                              lapply(data,
-                                     function(x) data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
-                                                            max.mean.speed = max.mean.speed(x[, speed.col.name],
-                                                                                            n = dur*sample.rate)))
-  )
-
-  set.two.max.mean <- do.call(rbind,
-                              lapply(data2,
-                                     function(x) data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
-                                                            max.mean.speed = max.mean.speed(x[, speed.col.name],
-                                                                                            n = dur*sample.rate)))
-  )
-
+                                     data2 = list(),
+                                     speed.col = "speed.col.name",
+                                     global.data = FALSE,
+                                     sample.rate = 10,
+                                     dur = 600) {
+  
+    if(global.data == TRUE) {
+    
+    
+   set.one.max.mean <- do.call(rbind,
+                               lapply(
+     data,
+     function(x) {
+       data.frame(duration = 1:nrow(x)/sample.rate,
+                  max.mean.speed = x[, speed.col])
+       }
+   )
+   )
+   
+   colnames(set.one.max.mean) <- c("duration", "max.mean.speed")
+   
+   
+    
+   set.two.max.mean <- do.call(rbind,
+                               lapply(
+     data2,
+     function(x) {
+       data.frame(duration = 1:nrow(x)/sample.rate,
+                  max.mean.speed = x[, speed.col])
+       }
+   )
+   )
+   
+   colnames(set.two.max.mean) <- c("duration", "max.mean.speed")
+    
   }
-
-  set.one.max.mean.global <- do.call(rbind,
-                                     lapply(
-                                       split(
-                                         set.one.max.mean, as.factor(set.one.max.mean$duration)
-                                       ),
-                                       function(x) {
-                                         return(x[which.max(x$max.mean.speed), ])
-                                       }
-                                     )
-  )
-
-  set.two.max.mean.global <- do.call(rbind,
-                                     lapply(
-                                       split(
-                                         set.two.max.mean, as.factor(set.two.max.mean$duration)
-                                       ),
-                                       function(x) {
-                                         return(x[which.max(x$max.mean.speed), ])
-                                       }
-                                     )
-  )
-
+  
+  
+  if(global.data == FALSE) { # this is the bottleneck (by far)
+    
+    # set.one.max.mean <- do.call(rbind,
+    #                             lapply(data,
+    #                                    function(x) data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
+    #                                                           max.mean.speed = max.mean.speed(x[, speed.col],
+    #                                                                                           n = dur*sample.rate)))
+    
+    set.one.max.mean <- data.frame()
+    for(i in seq_along(data)) {
+      
+      max.mean.speed.vect = max.mean.speed(data[[i]][, speed.col],
+                                           n = dur * sample.rate)
+      
+      temp.df <- data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
+                            max.mean.speed = max.mean.speed.vect)
+      
+      
+      set.one.max.mean <- rbind(set.one.max.mean, temp.df)
+      
+      
+    }
+    
+    set.two.max.mean <- data.frame()
+    for(j in seq_along(data2)) {
+      
+      max.mean.speed.vect2 = max.mean.speed(data2[[j]][, speed.col],
+                                           n = dur * sample.rate)
+      
+      temp.df <- data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
+                            max.mean.speed = max.mean.speed.vect2)
+      
+      
+      set.two.max.mean <- rbind(set.two.max.mean, temp.df)
+      
+      
+    }
+    
+    # set.two.max.mean <- do.call(rbind,
+    #                             lapply(data2,
+    #                                    function(x) data.frame(duration = seq(0.1, dur, by = 1/sample.rate),
+    #                                                           max.mean.speed = max.mean.speed(x[, speed.col],
+    #                                                                                           n = dur*sample.rate)))
+    # )
+    
+  }
+  
+  set.one.max.mean <- data.table(set.one.max.mean)
+  set.one.max.mean.global <- set.one.max.mean[, max(max.mean.speed, na.rm = TRUE), by = duration]
+  colnames(set.one.max.mean.global) <- c("duration", "max.mean.speed")
+  
+  set.two.max.mean <- data.table(set.two.max.mean)
+  set.two.max.mean.global <- set.two.max.mean[, max(max.mean.speed, na.rm = TRUE), by = duration]
+  colnames(set.two.max.mean.global) <- c("duration", "max.mean.speed")
+  
+  
   max.mean.compare <- list(set.one.max.mean.global,
                            set.two.max.mean.global)
-
+  
   max.mean.compare
-
+  
 }
