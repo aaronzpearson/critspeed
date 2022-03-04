963887f051c88f927970e9b4fcf254e09357faea
Aaron Pearson <46941570+aaronzpearson@users.noreply.github.com>
2022-03-04 11:41:03 -0700

    add cv.2 arg for min dur (120 s)

--- crit.speed.results.R
+++ crit.speed.results.R
@@ -1,59 +1,62 @@
 crit.speed.results <- function(player.speed,
                                global.data = FALSE,
                                sample.rate = 10,
+                               cv.2 = 120, # minimum duration (default = 2 minutes)
                                dur = 600,
                                d.prime.estim = 100,
                                crit.speed.estim = 3.5,
                                max.speed.estim = 12) {
-
-  if(global.data == FALSE) { # make 2 if statements for consistency in the package
-
+  
+  if(global.data == FALSE) { 
+    
     dat = max.mean.speed.df(player.speed = player.speed,
                             sample.rate = sample.rate,
                             dur = dur)
-
+    
   } else {
-
+    
     dat <- data.frame(max.mean.speed = player.speed,
                       duration = (1:length(player.speed))/ sample.rate)
-
+    
   }
-
+  
+  dat.cv2 <- subset(dat, duration >= cv.2)
+  
   extended.model <- cs.extended.model(data = dat,
                                       d.prime.estim = d.prime.estim,
                                       crit.speed.estim = crit.speed.estim,
                                       max.speed.estim = max.speed.estim)
-
+  
   five.param <- cs.five.param(data = dat,
                               crit.speed.estim = crit.speed.estim,
                               max.speed.estim = max.speed.estim)
-
+  
   three.param <- cs.three.param(data = dat,
                                 d.prime.estim = d.prime.estim,
                                 crit.speed.estim = crit.speed.estim,
                                 max.speed.estim = max.speed.estim)
-
-  two.param <- cs.two.param(data = dat,
+  
+  two.param <- cs.two.param(data = dat.cv2,
                             d.prime.estim = d.prime.estim,
                             crit.speed.estim = crit.speed.estim)
-
+  
   exponential.model <- cs.exponential(data = dat,
                                       crit.speed.estim = crit.speed.estim,
                                       max.speed.estim = max.speed.estim)
-
+  
   exponential.decay.model <- cs.exponential.decay(data = dat)
-
+  
   # inverse.model <- cs.inverse(data = dat,
   #                             d.prime.estim = d.prime.estim,
   #                             crit.speed.estim = crit.speed.estim)
-
+  
   cs.results <- data.frame(model = c("Exponential Decay",
                                      "Exponential Model",
                                      "Two Parameter",
                                      "Three Parameter",
                                      "Five Parameter",
                                      "OmniDomain"),
-
+                           
                            critical.speed = c(
                              coef(exponential.decay.model)[[3]],
                              coef(exponential.model)[[1]],
@@ -61,10 +64,10 @@ crit.speed.results <- function(player.speed,
                              coef(three.param)[[2]],
                              coef(five.param)[[4]],
                              coef(extended.model)[[2]]
-                                              ),
-
-
-
+                           ),
+                           
+                           
+                           
                            d.prime = c(
                              NA,
                              NA,
@@ -72,8 +75,8 @@ crit.speed.results <- function(player.speed,
                              coef(three.param)[[1]],
                              NA,
                              coef(extended.model)[[1]]
-                             ),
-
+                           ),
+                           
                            max.speed = c(
                              coef(exponential.decay.model)[[1]],
                              # predict(exponential.decay.model, newdata = data.frame(duration = 1)),
@@ -82,8 +85,8 @@ crit.speed.results <- function(player.speed,
                              coef(three.param)[[3]],
                              coef(five.param)[[5]],
                              coef(extended.model)[[3]]
-                             ),
-
+                           ),
+                           
                            rse = c(
                              summary(exponential.decay.model)[[3]],
                              summary(exponential.model)[[3]],
@@ -91,8 +94,8 @@ crit.speed.results <- function(player.speed,
                              summary(three.param)[[3]],
                              summary(five.param)[[3]],
                              summary(extended.model)[[3]]
-                             ),
-
+                           ),
+                           
                            rss = c(
                              summary(exponential.decay.model)[[3]]^2 * summary(exponential.decay.model)[[4]][2],
                              summary(exponential.model)[[3]]^2 * summary(exponential.model)[[4]][2],
@@ -101,7 +104,7 @@ crit.speed.results <- function(player.speed,
                              summary(five.param)[[3]]^2 * summary(five.param)[[4]][2],
                              summary(extended.model)[[3]]^2 * summary(extended.model)[[4]][2]
                            ),
-
+                           
                            aic = c(
                              AIC(exponential.decay.model),
                              AIC(exponential.model),
@@ -109,8 +112,8 @@ crit.speed.results <- function(player.speed,
                              AIC(three.param),
                              AIC(five.param),
                              AIC(extended.model)
-                             ),
-
+                           ),
+                           
                            bic = c(
                              BIC(exponential.decay.model),
                              BIC(exponential.model),
@@ -120,6 +123,6 @@ crit.speed.results <- function(player.speed,
                              BIC(extended.model)
                            )
   )
-
+  
   cs.results
 }
